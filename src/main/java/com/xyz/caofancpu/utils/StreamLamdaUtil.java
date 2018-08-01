0fb5aac68e8eb57e60b2486fbd1683c4ad55bf81
caofan <caofan@ovfintech.com>
2018-08-01 09:34:35 +0800

    add list2map util under steamlamda utils

--- src/main/java/com/xyz/caofancpu/utils/StreamLamdaUtil.java
+++ src/main/java/com/xyz/caofancpu/utils/StreamLamdaUtil.java
@@ -5,6 +5,7 @@ import com.xyz.caofancpu.model.Area;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,13 +15,19 @@ import java.util.stream.Stream;
  */
 public class StreamLamdaUtil {
     
-    /** 起始时间戳 */
+    /**
+     * 起始时间戳
+     */
     private static long startTime = 0L;
     
-    /** 结束时间戳 */
+    /**
+     * 结束时间戳
+     */
     private static long endTime = 0L;
     
-    /** 重启标识 */
+    /**
+     * 重启标识
+     */
     private static boolean restart = true;
     
     public static void main(String[] args) {
@@ -58,11 +65,11 @@ public class StreamLamdaUtil {
         List<Area> provinceList = initProvinceList();
         List<Area> cityList = initCityList();
         List<Area> countyList = initCountyList();
-    
+        
         // 市区根据pid分类
         Map<Integer, List<Area>> cityClassfiedMap = classify(cityList);
         Map<Integer, List<Area>> countyClassfiedMap = classify(countyList);
-    
+        
         // 织入省市区树状
         handleData(provinceList, cityClassfiedMap);
         handleData(cityList, countyClassfiedMap);
@@ -72,6 +79,7 @@ public class StreamLamdaUtil {
     
     /**
      * 将映射关系的数据塞入父类
+     *
      * @param parentList
      * @param childMap
      */
@@ -81,20 +89,27 @@ public class StreamLamdaUtil {
     
     /**
      * 利用流的自动分类功能
+     *
      * @param areaList
      * @return
      */
     public static Map<Integer, List<Area>> classify(List<Area> areaList) {
         // 方式一：顺序流处理，处理结果保持入参的顺序
         return areaList.stream().collect(Collectors.groupingBy(Area::getPid));
-        
+
 //        // 方式二：并行流处理，通过多线程，map-reduce；但是注意：并行流使用在大数据量场景下，否则性能反而低
 //        return areaList.parallelStream().collect(Collectors.groupingBy(Area::getPid));
+    }
+    
+    public Map<Integer, Area> concertToMap(List<Area> areaList) {
+        Map<Integer, Area> resultMap = areaList.stream().collect(Collectors.toMap(Area::getId, Function.identity()));
+        return resultMap;
     }
     
     /**
      * 初始化省的数据
      * 可用查询数据库替代
+     *
      * @return
      */
     public static List<Area> initProvinceList() {
