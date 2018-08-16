3f9e2ce3bea0093038ca2092e193e49b1edde801
caofan <caofan@ovfintech.com>
2018-08-16 18:26:53 +0800

    add swagger2 config

--- src/main/java/com/xyz/caofancpu/DebuggerKingApplication.java
+++ src/main/java/com/xyz/caofancpu/DebuggerKingApplication.java
@@ -8,14 +8,16 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.Resource;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.xyz.caofancpu"})
 //禁止自动注册
 //@EnableDiscoveryClient
 @MapperScan("com.xyz.caofancpu.mapper")
 @EnableEncryptableProperties
+@EnableSwagger2
 public class DebuggerKingApplication {
     
     @Resource
---
+++ src/main/java/com/xyz/caofancpu/config/Swagger2Config.java
@@ -0,0 +1,35 @@
+package com.xyz.caofancpu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("客户准入微服务api文档")
+                .description("客户准入api文档")
+                .version("1.0.0")
+                .build();
+    }
+
+
+}
