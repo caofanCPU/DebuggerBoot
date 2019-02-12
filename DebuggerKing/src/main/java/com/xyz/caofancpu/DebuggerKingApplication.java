22c6bff32ae3bfa8c0b399e8484e78164c5d16d8
caofan <caofan@ovfintech.com>
2019-02-12 18:50:01 +0800

    optimize code

--- DebuggerKing/src/main/java/com/xyz/caofancpu/DebuggerKingApplication.java
+++ DebuggerKing/src/main/java/com/xyz/caofancpu/DebuggerKingApplication.java
@@ -5,13 +5,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import javax.annotation.Resource;
 
 @SpringBootApplication(scanBasePackages = {"com.xyz.caofancpu"})
 //禁止自动注册
@@ -31,22 +25,19 @@ public class DebuggerKingApplication {
         ParserConfig.getGlobalInstance().addAccept("com.ovfintech.bynz.model");
     }
     
-    @Resource
-    RestTemplateBuilder restTemplateBuilder;
-    
     public static void main(String[] args) {
         SpringApplication.run(DebuggerKingApplication.class, args);
     }
-    
-    /**
-     * 注意：@LoadBalanced注解，使用该注解，则调用其他服务时，必须使用服务名称[http://SERVICE-XXX]，而非IP:port
-     * 使用场景: 都在网关内部, 走ribbon调用
-     *
-     * @return
-     */
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return restTemplateBuilder.build();
-    }
+
+//    /**
+//     * 注意：@LoadBalanced注解，使用该注解，则调用其他服务时，必须使用服务名称[http://SERVICE-XXX]，而非IP:port
+//     * 使用场景: 都在网关内部, 走ribbon调用
+//     *
+//     * @return
+//     */
+//    @Bean
+//    @LoadBalanced
+//    RestTemplate restTemplate() {
+//        return restTemplateBuilder.build();
+//    }
 }
