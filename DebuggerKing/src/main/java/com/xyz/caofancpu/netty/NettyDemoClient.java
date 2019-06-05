59a1664c6bf5bc30a351e5db5c373bd41730b499
曹繁 <caofan@huitongjy.com>
2019-06-05 11:27:42 +0800

    add netty demo for open the gate of NIO world!

---
+++ DebuggerKing/src/main/java/com/xyz/caofancpu/netty/NettyDemoClient.java
@@ -0,0 +1,46 @@
+package com.xyz.caofancpu.netty;
+
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Date;
+
+/**
+ * Netty 客户端DEMO
+ *
+ * @author DebuggerKing
+ */
+public class NettyDemoClient {
+    public static void main(String[] args) {
+        String serverHost = "127.0.0.1";
+        int serverPort = 8000;
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel channel) {
+                        channel.pipeline().addLast(new StringEncoder());
+                    }
+                });
+        
+        Channel channel = bootstrap.connect(serverHost, serverPort).channel();
+        
+        while (true) {
+            channel.writeAndFlush(new Date() + ": Hello My King!");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+}
---
+++ DebuggerKing/src/main/java/com/xyz/caofancpu/netty/NettyDemoServer.java
@@ -0,0 +1,42 @@
+package com.xyz.caofancpu.netty;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * Netty 服务端DEMO
+ *
+ * @author DebuggerKing
+ */
+public class NettyDemoServer {
+    
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        int serverPort = 8000;
+        NioEventLoopGroup boos = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        
+        serverBootstrap.group(boos, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) {
+                        nioSocketChannel.pipeline().addLast(new StringDecoder());
+                        nioSocketChannel.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) {
+                                System.out.println(channelHandlerContext.name() + "输出: " + s);
+                            }
+                        });
+                    }
+                })
+                .bind(serverPort);
+    }
+}
