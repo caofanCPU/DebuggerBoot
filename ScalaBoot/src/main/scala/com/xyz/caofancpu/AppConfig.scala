4064110270900e558ccca3f956afb5ce2f400bdf
曹繁 <caofan@huitongjy.com>
2019-07-16 11:32:57 +0800

    remove rare project directories

--- ScalaBoot/src/main/scala/com/xyz/caofancpu/AppConfig.scala
+++
@@ -1,7 +0,0 @@
-package com.xyz.caofancpu
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication(scanBasePackages = Array("com.xyz.caofancpu"))
-//@EnableEncryptableProperties
-class AppConfig
