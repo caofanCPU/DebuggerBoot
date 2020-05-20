81d6a092876e2a069a15d7fb8f1881431cba0f75
caofan <caofan@huitongjy.com>
2020-05-20 15:38:08 +0800

    logger adjust

--- DebuggerKing/src/main/java/com/xyz/caofancpu/schedule/ScheduleProcessor.java
+++ DebuggerKing/src/main/java/com/xyz/caofancpu/schedule/ScheduleProcessor.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.Date;
-
 /**
  * Created by caofanCPU on 2018/7/20.
  */
@@ -25,7 +23,7 @@ public class ScheduleProcessor {
     @Scheduled(cron = "0 0/1 * * * ?")
     public void doSchedule() {
         log.info("\n"
-                + DateUtil.parseJavaUtilDate(new Date(), DateUtil.DATETIME_FORMAT_SIMPLE)
+                + DateUtil.getCurrentDateTime(DateUtil.DATETIME_FORMAT_SIMPLE)
                 + ".007  帝八哥 2018 --- [           Debugger] K.I.N.G :"
                 + " Demo定时任务第[" + count + "]次执行...");
         count++;
@@ -36,15 +34,15 @@ public class ScheduleProcessor {
      */
     private void outNormalCronExpression() {
         NormalUseForTestUtil.out("Cron表达式范例：\n" +
-                "             每隔5秒执行一次：*/5 * * * * ?\n" +
-                "             每隔1分钟执行一次：0 */1 * * * ?\n" +
-                "             每天23点执行一次：0 0 23 * * ?\n" +
-                "             每天凌晨1点执行一次：0 0 1 * * ?\n" +
-                "             每月1号凌晨1点执行一次：0 0 1 1 * ?\n" +
-                "             每月最后一天23点执行一次：0 0 23 L * ?\n" +
-                "             每周星期天凌晨1点实行一次：0 0 1 ? * L\n" +
-                "             在26分、29分、33分执行一次：0 26,29,33 * * * ?\n" +
-                "             每天的0点、13点、18点、21点都执行一次：0 0 0,13,18,21 * * ?\n"
+                "             每隔5秒执行一次:                  */5 * * * * ?\n" +
+                "             每隔5分钟执行一次:                0 */5 * * * ?\n" +
+                "             每天23点执行一次:                 0 0 23 * * ?\n" +
+                "             每天凌晨2点执行一次:               0 0 2 * * ?\n" +
+                "             每月2号凌晨4点执行一次：            0 0 2 4 * ?\n" +
+                "             每月最后一天23点执行一次：          0 0 23 L * ?\n" +
+                "             每周星期天凌晨2点实行一次：          0 0 2 ? * L\n" +
+                "             在26分、29分、33分执行一次：        0 26,29,33 * * * ?\n" +
+                "             每天的0点、22点、23点都执行一次：    0 0 0 0,22,23 * * ?\n"
         );
     }
 
