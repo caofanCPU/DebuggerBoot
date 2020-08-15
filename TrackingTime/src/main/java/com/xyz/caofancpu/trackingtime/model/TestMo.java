4d608c6c5596466dac5294330462c9895c97f67f
D8GER <xyb5to0ZCY@gmail.com>
2020-08-15 21:57:56 +0800

    use new version generate code

---
+++ TrackingTime/src/main/java/com/xyz/caofancpu/trackingtime/model/TestMo.java
@@ -0,0 +1,39 @@
+package com.xyz.caofancpu.trackingtime.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * UserInfoMo
+ *
+ * @author Power+
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class TestMo {
+
+    /**
+     * ID 用户唯一编码
+     */
+    private Long id;
+
+    /**
+     * name 姓名
+     */
+    private String name;
+
+    /**
+     * age 年龄
+     */
+    private Integer age;
+
+    /**
+     * job 工作
+     */
+    private String job;
+
+}
